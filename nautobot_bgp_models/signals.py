2eda701de3723b2283aedd93115a1f86fab0c285
Damien Garros <dgarros@gmail.com>
2021-08-31 15:48:18 -0400

    Add error handling if status is not present

--- nautobot_bgp_models/signals.py
+++ nautobot_bgp_models/signals.py
@@ -74,7 +74,12 @@ def post_migrate_create_statuses(sender, apps, **kwargs):
         ContentType = apps.get_model("contenttypes", "ContentType")
         ct_model = ContentType.objects.get_for_model(model)
         for slug in default_statuses:
-            status = Status.objects.get(slug=slug)
+            try:
+                status = Status.objects.get(slug=slug)
+            except Status.DoesNotExist:
+                print(f"nautobot_bgp_models: Unable to find status: {slug} .. SKIPPING")
+                continue
+
             if ct_model not in status.content_types.all():
                 status.content_types.add(ct_model)
                 status.save()
