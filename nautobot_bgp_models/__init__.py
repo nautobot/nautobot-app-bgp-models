7ba81ef27ffa0f12489cd9c8ef1ec6448914c920
Stephen Kiely <stephenkiely87@gmail.com>
2025-04-16 21:30:02 -0500

    Initial prep for Nautobot 3.0
    Currently setting the minimum version of Nautobot to 2.4 as 3.0.0 as breaking changes are unknown. We will likely need to change this, but for now this helps us to be able to update to the Component Framework.

--- nautobot_bgp_models/__init__.py
+++ nautobot_bgp_models/__init__.py
@@ -19,8 +19,8 @@ class NautobotBGPModelsConfig(NautobotAppConfig):
     description = "Nautobot BGP Models App."
     base_url = "bgp"
     required_settings = []
-    min_version = "2.0.3"
-    max_version = "2.9999"
+    min_version = "2.4.0"
+    max_version = "3.9999"
     default_settings = {
         "default_statuses": {
             "AutonomousSystem": ["Active", "Available", "Planned"],
